d238ca1e6e705f1a3d62cb908bef049cabbef082
JuiceYY <xryuan@stu.xidian.edu.cn>
2019-06-02 11:55:55 +0800

    增加一个页面

---
+++ app/src/main/java/cn/istary/acmcoder/view/activity/HelpActivity.java
@@ -0,0 +1,15 @@
+package cn.istary.acmcoder.view.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cn.istary.acmcoder.R;
+
+public class HelpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+    }
+}
--- app/src/main/java/cn/istary/acmcoder/view/fragment/MineFragment.java
+++ app/src/main/java/cn/istary/acmcoder/view/fragment/MineFragment.java
@@ -1,5 +1,7 @@
 package cn.istary.acmcoder.view.fragment;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,9 +9,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import cn.istary.acmcoder.R;
+import cn.istary.acmcoder.view.activity.HelpActivity;
 
 /*
  * CREATED BY: Sinry
@@ -19,9 +24,14 @@ import cn.istary.acmcoder.R;
 
 public class MineFragment extends Fragment implements View.OnClickListener {
 
+    private int pressCount = 0;
+    private long pressTime = 0;
+
     private View favIcon;
     private View followIcon;
     private View callIcon;
+
+    private final static int[] toastColors = new int[]{Color.BLUE, Color.RED, Color.YELLOW, Color.GREEN, Color.MAGENTA};
 
     @Nullable
     @Override
@@ -38,6 +48,45 @@ public class MineFragment extends Fragment implements View.OnClickListener {
 
     @Override
     public void onClick(View v) {
-        Toast.makeText(getActivity(), "敬请期待", Toast.LENGTH_SHORT).show();
+        switch (v.getId()) {
+            case R.id.item_mine_favourite:
+            case R.id.item_mine_follow:
+                Toast.makeText(getActivity(), "敬请期待", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.item_mine_contract:
+                startEggs();
+                break;
+
+            default:
+                break;
+        }
+
+    }
+
+    private void startEggs() {
+        long time = System.currentTimeMillis();
+        if (time - pressTime > 1000) {
+            pressCount = 1;
+            pressTime = time;
+            Toast.makeText(getActivity(), "敬请期待", Toast.LENGTH_SHORT).show();
+        } else {
+            pressCount++;
+            pressTime = time;
+            if (pressCount < 3) {
+                Toast.makeText(getActivity(), "敬请期待", Toast.LENGTH_SHORT).show();
+            }else if(pressCount < 8){
+                Toast toast = Toast.makeText(getActivity(), "敬请期待", Toast.LENGTH_SHORT);
+                LinearLayout layout = (LinearLayout) toast.getView();
+                TextView v = toast.getView().findViewById(android.R.id.message);
+                v.setTextColor(toastColors[pressCount-3]);
+            }else{
+                Toast.makeText(getActivity(), "恭喜你发现了隐藏页面", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getActivity(), HelpActivity.class);
+                startActivity(intent);
+            }
+
+        }
+
+
     }
 }
